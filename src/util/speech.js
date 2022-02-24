1fa0b888f427a72692b301a0e321874665a9f991
jgmts33 <jorgreemts@gmail.com>
2022-02-23 21:13:17 -0500

    Add speak-tts helper library

--- src/util/speech.js
+++ src/util/speech.js
@@ -1,4 +1,22 @@
-export default (message) => {
-    const speech = new SpeechSynthesisUtterance(message)
-    window.speechSynthesis?.speak(speech)
+import Speech from 'speak-tts'
+
+let speech = null
+try {
+    speech = new Speech()
+} catch {
+    console.warn('Speech synthesis not supported.')
+}
+
+export default async (text, opts) => {
+    if (!speech) return
+    try {
+        const data = await speech.init({
+            lang: 'en-US',
+            ...opts,
+        })
+        speech.setVoice(data.voices[0].name)
+        speech.speak({ text })
+    } catch {
+        console.warn('Failed to speak!')
+    }
 }

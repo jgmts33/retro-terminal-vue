20e9ba7a35e610d86d012646380cede39f71df54
jgmts33 <jorgreemts@gmail.com>
2022-02-28 14:33:55 -0500

    Offer to start an email

--- src/bin/contact.js
+++ src/bin/contact.js
@@ -3,4 +3,7 @@ import { EMAIL, FIRST_NAME, PRONOUN_OBJECT } from '@/config'
 export default async ({ output, confirm }) => {
     await output(`${FIRST_NAME} can be reached at:`)
     await output(EMAIL, { delay: 0, speak: `${FIRST_NAME} say pan at fast mail dot com` })
+    if (await confirm(`Would you like to email ${PRONOUN_OBJECT} now?`)) {
+        window.open(`mailto:${EMAIL}`, '_blank')
+    }
 }

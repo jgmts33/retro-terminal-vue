d53644c459811b52986de00a8965776387046fc2
jgmts33 <jorgreemts@gmail.com>
2022-03-01 16:32:18 -0500

    Update help layout

--- src/bin/help.js
+++ src/bin/help.js
@@ -1,3 +1,51 @@
+import { FIRST_NAME } from '@/config'
+
+const sections = [
+    {
+        name: `Info about ${FIRST_NAME}`,
+        commands: [
+            'about',
+            'contact',
+            'github',
+            'resume',
+        ],
+    },
+
+    {
+        name: 'Games',
+        commands: [
+            'retrodle',
+            'trampoline',
+        ],
+        pronounciations: [
+            'retro-dull',
+            'trampoline',
+        ],
+    },
+
+    {
+        name: 'Utilities',
+        commands: [
+            'cake',
+            'clear',
+            'echo',
+            'eightball',
+            'fire',
+            'joke',
+            'missingno',
+            'python',
+            'zen',
+        ],
+    },
+]
+
 export default async ({ output }) => {
-    await output('Available commands: about, antigravity, cake, clear, contact, echo, eightball, fire, github, help, joke, missingno, reboot, retrodle, trampoline, zen')
+    await output('Available commands...\n\n')
+
+    for (const [idx, section] of sections.entries()) {
+        await Promise.all([
+            output(`${section.name}:`, { speak: false }),
+            output(`${section.commands.join(', ')}${idx < sections.length - 1 ? '\n\n' : ''}`, { speak: (section.pronounciations || section.commands).join(', '), speechOptions: { rate: 1.5 } }),
+        ])
+    }
 }

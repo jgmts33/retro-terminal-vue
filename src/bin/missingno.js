0d6439891d61322a6c093e2132c565ed069e37bc
jgmts33 <jorgreemts@gmail.com>
2022-03-01 14:41:47 -0500

    Add more content to missingno command

--- src/bin/missingno.js
+++ src/bin/missingno.js
@@ -1,6 +1,18 @@
+import sleep from 'sleep-promise'
 import store from '@/store'
 
-export default async ({ output }) => {
-    store.commit('toggleGlitching')
-    await output(store.state.glitching ? '0x39FC2 0x39FDD' : 'Normality restored.')
+export default async ({ output, confirm }) => {
+    await output('Wild MISSINGNO. appeared!', { speak: 'Wild missing no appeared!' })
+    if (await confirm('Throw a poké ball at it?')) {
+        await output('You used poké ball!')
+        await output('...')
+        await output('...')
+        await output('...')
+        await sleep(2)
+        store.commit('toggleGlitching')
+        await output('Hooray, MISSINGNO. was caught!', { speak: 'Hooray, missing no was caught!', delay: 0 })
+    } else {
+        await output("That's probably for the best.")
+        await output('The wild MISSINGNO. fled.', { speak: 'The wild missing no fled.' })
+    }
 }

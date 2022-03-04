4c7f103696fe17ee6185f7ee374a3067f0f3a014
jgmts33 <jorgreemts@gmail.com>
2022-03-04 11:49:22 -0500

    Implement skipping typing animations and speech

--- src/components/Kernel.vue
+++ src/components/Kernel.vue
@@ -3,8 +3,6 @@ stdio(:history='history' :inputting='!!resolveInput' @submit='handleInput')
 </template>
 
 <script>
-import sleep from 'sleep-promise'
-
 import Stdio from '@/components/Stdio'
 import speak from '@/util/speech'
 
@@ -28,25 +26,62 @@ export default {
     },
 
     methods: {
-        async output(message, opts) {
-            opts = {
-                speak: true,
-                speakOptions: null,
-                delay: 1000,
-                speed: 45,
-                ...opts,
-            }
-            await sleep(opts.delay)
-            if (opts.speak) speak(opts.speak === true ? message : opts.speak, opts.speechOptions, this.instanceKey)
-            const line = { input: false, message: opts.speed ? '' : message }
-            this.history.push(line)
-            if (opts.speed) {
-                const lineIdx = this.history.length - 1
-                for (let c = 0; c < message.length; c++) {
-                    this.history[lineIdx] = { ...line, message: this.history[lineIdx].message + message[c] }
-                    await sleep(opts.speed)
+        output(message, opts) {
+            return new Promise((resolve) => {
+                // Apply defaults
+                opts = {
+                    speak: true,
+                    speakOptions: null,
+                    delay: 1000,
+                    speed: 45,
+                    ...opts,
                 }
-            }
+
+                const outputHandler = () => {
+                    // Speak the line
+                    let cancelSpeech = null
+                    if (opts.speak) cancelSpeech = speak(opts.speak === true ? message : opts.speak, opts.speechOptions, this.instanceKey)
+
+                    // Add a new empty line (or the completed line if speed was unset)
+                    const line = { input: false, message: opts.speed ? '' : message }
+                    this.history.push(line)
+                    if (!opts.speed) return resolve()
+
+                    // Prepare to type the text to the screen
+                    const lineIdx = this.history.length - 1
+                    let cIdx = 0
+                    let typeInterval = null
+
+                    // Allow hitting any key to skip the typing animation
+                    const onKeyDown = () => {
+                        this.history[lineIdx] = { ...line, message }
+                        if (cancelSpeech) cancelSpeech()
+                        clearInterval(typeInterval)
+                        window.removeEventListener('keydown', onKeyDown)
+                        resolve()
+                    }
+
+                    // "Type" the text to the screen
+                    const typeCharacter = () => {
+                        this.history[lineIdx] = { ...line, message: this.history[lineIdx].message + message[cIdx] }
+                        cIdx += 1
+                        if (cIdx === message.length) {
+                            clearInterval(typeInterval)
+                            window.removeEventListener('keydown', onKeyDown)
+                            resolve()
+                        }
+                    }
+                    typeInterval = setInterval(typeCharacter, opts.speed)
+                    window.addEventListener('keydown', onKeyDown)
+                }
+
+                // Apply the delay if there is one
+                if (opts.delay) {
+                    setTimeout(outputHandler, opts.delay)
+                } else {
+                    outputHandler()
+                }
+            })
         },
 
         print(...messages) {
--- src/util/speech.js
+++ src/util/speech.js
@@ -9,17 +9,19 @@ try {
     console.warn('Speech synthesis not supported.')
 }
 
-export default async (text, opts, instanceKey) => {
+export default (text, opts, instanceKey) => {
     // Skip if speech isn't available, sound is off, or this is from a program that was escaped
     if (!speech || !store.state.sound || (instanceKey && instanceKey !== store.state.instanceKey)) return
 
     try {
-        const data = await speech.init({
+        speech.init({
             lang: 'en-US',
             ...opts,
+        }).then((data) => {
+            speech.setVoice(data.voices[0].name)
+            speech.speak({ text })
         })
-        speech.setVoice(data.voices[0].name)
-        speech.speak({ text })
+        return () => speech.cancel()
     } catch {
         console.warn('Failed to speak!')
     }
